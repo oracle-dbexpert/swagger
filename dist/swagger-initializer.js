f787cc410cb635e7e2912908ec0ad11f2c2bbbbc
rgx <reginaruizfloress@gmail.com>
2026-02-03 19:12:53 -0600

    restoring dist files

---
+++ dist/swagger-initializer.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    urls: [
+      {url: "oracle-db-services.yaml", name: "OCI Database Services Module"},
+    //  {url: "https://apexadb-dev.oracle.com/ords/dbexpert/open-api-catalog/pricing/", name: "Database Pricing Module"},
+    ],
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
